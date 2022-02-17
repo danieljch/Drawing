7d0bb12d2c9e124181bf29650f0695094baba2ad
Daniel JCH <danielj.callisayah@gmail.com>
2022-02-17 16:36:29 -0400

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 17/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
