a7d0f7828de0489748be53a5928a78b76a8faa7e
Daniel JCH <danielj.callisayah@gmail.com>
2022-02-17 16:51:28 -0400

    Drawing an Arc like struct

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Triangle()
-            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        ZStack {
+            Triangle()
+                .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
             .frame(width: 200, height: 300)
+            Arc(startAngle: .degrees(00), endAngle: .degrees(180), clockwise: false)
+                .stroke(.blue, lineWidth: 10)
+                .frame(width: 300, height: 300)
+        }
      
-        
+    
     }
 }
 
@@ -29,6 +34,20 @@ struct Triangle: Shape {
         return path
     }
 }
+
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+
+        return path
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
