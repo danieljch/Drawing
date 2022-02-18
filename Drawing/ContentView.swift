7260118072791bd3d4e3306447f8f53dcdd17398
Daniel JCH <danielj.callisayah@gmail.com>
2022-02-18 12:30:23 -0400

    Circles effect drawing with metal

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,21 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
 
     var body: some View {
         VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
 
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
+            Slider(value: $colorCycle)
         }
     }
 }
@@ -80,7 +73,7 @@ struct Flower: Shape {
         var path = Path()
 
         // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+        for number in stride(from: 0, to: Double.pi * 2 , by: Double.pi / 8) {
             // rotate the petal by the current value of our loop
             let rotation = CGAffineTransform(rotationAngle: number)
 
@@ -99,6 +92,41 @@ struct Flower: Shape {
 
         // now send the main path back
         return path
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }.drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
