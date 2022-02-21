212d5e6aac5ef08b99acfdfffb75a39a1219b580
Daniel JCH <danielj.callisayah@gmail.com>
2022-02-21 17:30:16 -0400

    first example of animatableData

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,15 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var colorCycle = 0.0
-
+    @State private var insetAmount = 50.0
     var body: some View {
-        VStack {
-            ColorCyclingCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
+           Trapezoid(insetAmount: insetAmount)
+               .frame(width: 200, height: 100)
+               .onTapGesture {
+                   withAnimation {
+                       insetAmount = Double.random(in: 10...90)
+                   }
+               }
+       }
+}
 
-            Slider(value: $colorCycle)
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
         }
+
+        return path
     }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
 }
 
 struct Triangle: Shape {
